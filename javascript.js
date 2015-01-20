86d81ac9779e6f83b3d5cb4b59a3c19824bc3df7
David Sok <sokd88@gmail.com>
2015-01-20 12:48:45 -0500

    lesson3-functions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
-var foo = doSomething(2);
-var bar = doSomething(3)
-
-function doSomething(paramOne) {
+var doSomething = function(paramOne, paramTwo, fn) {
   paramOne = paramOne +3;
   paramOne = paramOne + 1;
   paramOne = paramOne * 8;
 
-  return paramOne;
+  return fn(paramOne, paramTwo);
+};
+
+function sum(paramOne, paramTwo){
+  return paramOne + paramTwo;
 }
+
+var foo = doSomething(2, 2, sum);
+var bar = doSomething(3, 2, function(paramOne, paramTwo){
+  return paramOne * paramTwo;
+});
 
 alert(foo);
 alert(bar);
