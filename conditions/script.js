97d73b2c865e88ef7566ea1b4b4798c0c518214d
David Sok <sokd88@gmail.com>
2015-01-21 18:18:15 -0500

    conditionals

---
+++ conditions/script.js
@@ -0,0 +1,34 @@
+var foo = "hello"
+  bar = 5
+
+//logical AND is &&
+//T && T = true
+//T && F = false
+//F && T = false
+//F && F = false
+
+//logical OR is ||
+//T || T = true
+//T || F = true
+//F || T = true
+//F || F = false
+
+//logical NOT is !
+// !T = false
+// !F = true
+
+// if (!(foo !== "hello")){
+//   alert("true");
+// }
+
+//alert("This is outside the if statement");
+
+//Truthy and Falsy values
+//0 is falsy, all other numbers are truthy
+//"" empty string is falsy, other strings are truthy
+//[] arrays are truthy
+//{} objects are truthy
+//null is a falsy value, null object
+if () {
+  alert("hi");
+}
