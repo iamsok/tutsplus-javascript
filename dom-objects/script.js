f717cb710e232ad2e8848a6905f90f05279e4bb1
David Sok <sokd88@gmail.com>
2015-01-23 16:17:42 -0500

    dom objects, using queryselector

---
+++ dom-objects/script.js
@@ -0,0 +1,6 @@
+(function(){
+
+var pElement = document.querySelector("#foo");
+    alert(pElement.parentNode.tagName);
+
+}());
--- window-objects/script.js
+++ window-objects/script.js
@@ -1,20 +1,20 @@
-// var foo = "hello, window";
+var foo = "hello, window";
 
-// var bar = function(){
-//   var foo = "hello, function";
+var bar = function(){
+  var foo = "hello, function";
 
-//   alert(foo);
-// }
-
-// bar();
-
-
-(function() {
-
-if (confirm("Do you want to go to google.com?")){
-  alert("We will take you there");
-} else {
-  alert("Stay here");
+  alert(foo);
 }
 
-}());
+bar();
+
+
+// (function() {
+
+// if (confirm("Do you want to go to google.com?")){
+//   alert("We will take you there");
+// } else {
+//   alert("Stay here");
+// }
+
+// }());
